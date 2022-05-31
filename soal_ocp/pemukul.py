dc59b3081df493a3c62e01af08447c55b508934d
harief285 <harief285@gmail.com>
2022-05-31 15:11:22 +0700

    refisi relasi diargam open close

--- soal_ocp/pemukul.py
+++ soal_ocp/pemukul.py
@@ -3,7 +3,7 @@ from karakter import Karakter
 class Penembak(Karakter):
     
     def nama (self, karakter: Karakter)->str:
-        return ("NAMA",karakter.get_nama)
+        return ("NAMA",karakter())
 
     def power (self, karakter: Karakter)-> float:
-        return ("POWERRRRR",karakter.get_power)
+        return ("POWERRRRR",karakter())
