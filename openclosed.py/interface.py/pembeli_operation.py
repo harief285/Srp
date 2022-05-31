0caa94efcdaf57443da37a41700ecff1258849d4
rayhangrdp <mr2021649@gmail.com>
2022-05-31 11:50:04 +0700

    rayhan

---
+++ openclosed.py/interface.py/pembeli_operation.py
@@ -0,0 +1,6 @@
+from abc import ABC 
+
+class PembeliOperation(ABC):
+    
+    def memesan_pesanam(self)-> None:
+        pass
--- soal/balok.py
+++ soal/balok.py
@@ -12,15 +12,16 @@ class Balok:
     def set_panjang(panjang:float):
         self.__panjang-panjang
     
-        def get_lebar(self)-> float:
-            return self.__lebar
+    def get_lebar(self)-> float:
+        return self.__lebar
     
     def set_lebar(lebar:float):
         self.__lebar=lebar
     
-        def get_tinggi(self)-> float:
-            return self.__tinggi
+    def get_tinggi(self)-> float:
+        return self.__tinggi
     
     
     def set_tinggi(tinggi:float): 
-        self.__tinggi=tinggi 
+        self.__tinggi=tinggi \\\\
+            
