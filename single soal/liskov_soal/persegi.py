02b792b080f5022f349f9e1eec130ef6cafc6ecf
rayhangdrp <mr2021649@gmail.com>
2022-05-31 21:26:27 +0700

    rayhan

---
+++ single soal/liskov_soal/persegi.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Persegi(PerhitungBangunDatar):
+    
+        
+    def hitung_keliling(self,persegi:Persegi)->float:
+        return 2*((persegi.get_panjang()+persegi.get_lebar()))
+    
+    def hitung_luas(self,persegi:Persegi)->float:
+        return((persegi.get_panjang()*persegi.get_lebar()))
