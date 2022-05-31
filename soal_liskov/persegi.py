069c6ca42520a4bba2f3382d48c4cbf3e9b3802e
harief285 <harief285@gmail.com>
2022-05-31 21:06:45 +0700

    arief soal liskov

---
+++ soal_liskov/persegi.py
@@ -0,0 +1,10 @@
+from perhitungandatar import *
+
+class Persegi(PenghitungBangunDatar):
+    def __init__(self, sisi: float):
+        super().__init__(sisi)
+    def hitung_luas(self):
+        luas = self.get_sisi()*self.get_sisi()
+        print("Hasil Luas Persegi : ",luas)
+    def hitung_keliling(self):
+        print("Hasil Keliling Persegi : ",self.get_sisi()*4)
