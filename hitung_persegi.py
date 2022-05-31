8f4faef4c40af6e3d912188310e1312c136179a6
bowoxdaily <ikhwanudinaldi04@gmail.com>
2022-05-31 13:50:04 +0700

    bowo

--- hitung_persegi.py
+++
@@ -1,6 +0,0 @@
-from persegi import Persegi
-class Hitung:
-    def hitung_luas(self,persegi : Persegi)->float:
-        return persegi.get_sisi()*persegi.get_sisi()
-    def hitung_keliling(self,persegi : Persegi)->float:
-        return persegi.get_sisi()*4
--- persegi_view.py
+++
@@ -1,7 +0,0 @@
-from hitung_persegi import Hitung
-from persegi import Persegi
-class Show:
-    def show_luas(self,persegi :Persegi , persegi_hitug : Hitung):
-        print(persegi_hitug.hitung_luas(persegi))
-    def show_keliling(self,persegi :Persegi , persegi_hitug : Hitung):
-        print(persegi_hitug.hitung_keliling(persegi))
